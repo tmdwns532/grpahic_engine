8821573149ff29f3b5d9af08260c03e5dcd3bce1
DESKTOP-N5MBFLU\tmdwn <202213140@chungkang.academy>
2025-06-18 22:03:58 +0900

    물 스크립트 오류 수정 완료, 문제없이 실행 가능

---
+++ Assets/WaterWorks/Scripts/Water_Volume.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class Water_Volume : ScriptableRendererFeature
+{
+    class CustomRenderPass : ScriptableRenderPass
+    {
+        public RenderTargetIdentifier source;
+        private Material _material;
+
+        private RTHandle tempRenderTarget;
+        private RTHandle tempRenderTarget2;
+
+        public CustomRenderPass(Material mat)
+        {
+            _material = mat;
+
+            tempRenderTarget = RTHandles.Alloc("_TemporaryColourTexture", name: "_TemporaryColourTexture");
+            tempRenderTarget2 = RTHandles.Alloc("_TemporaryDepthTexture", name: "_TemporaryDepthTexture");
+        }
+
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+            RenderingUtils.ReAllocateIfNeeded(ref tempRenderTarget, cameraTextureDescriptor, FilterMode.Bilinear, TextureWrapMode.Clamp, name: "_TemporaryColourTexture");
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            if (renderingData.cameraData.cameraType != CameraType.Reflection)
+            {
+                CommandBuffer cmd = CommandBufferPool.Get("Water Volume Pass");
+
+                var cameraTarget = renderingData.cameraData.renderer.cameraColorTargetHandle;
+
+                // Blit을 대체하여 사용
+                Blitter.BlitCameraTexture(cmd, cameraTarget, tempRenderTarget, _material, 0);
+                Blitter.BlitCameraTexture(cmd, tempRenderTarget, cameraTarget);
+
+                context.ExecuteCommandBuffer(cmd);
+                CommandBufferPool.Release(cmd);
+            }
+        }
+
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+            // 필요하면 여기에 RTHandle 해제 코드 추가 가능
+        }
+    }
+
+    [System.Serializable]
+    public class _Settings
+    {
+        public Material material = null;
+        public RenderPassEvent renderPass = RenderPassEvent.AfterRenderingSkybox;
+    }
+
+    public _Settings settings = new _Settings();
+    CustomRenderPass m_ScriptablePass;
+
+    public override void Create()
+    {
+        if (settings.material == null)
+        {
+            settings.material = (Material)Resources.Load("Water_Volume");
+        }
+
+        m_ScriptablePass = new CustomRenderPass(settings.material)
+        {
+            renderPassEvent = settings.renderPass
+        };
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        renderer.EnqueuePass(m_ScriptablePass);
+    }
+}
